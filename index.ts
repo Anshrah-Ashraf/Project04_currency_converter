89f2b24746da9c9423f9edda8a38766a5632a45d
Anshrah Ashraf <159923970+Anshrah-Ashraf@users.noreply.github.com>
2024-07-22 01:22:27 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,62 @@
+import inquirer from "inquirer";
+//Define the list of currencies and their exchange rates
+
+let exchangeRates: {[key: string]: number} = {
+    "USD": 1,       //Base currency
+    "EUR": 0.9,     //European Currency (Euro)
+    "JPY": 113,     //Japanese Currency (Yen)
+    "CAD": 1.3,     //Canadian Currency (Dollar)
+    "AUD":  1.65,   //Australian Currency (Dollar)
+    "PAK": 280,     //Pakistani Currency (Ruppee)
+    //Add more currencies and their exchange rates here
+};
+
+//Promt the user to select currencies from and to
+let userAnswer = await inquirer.prompt<{from_currency:string; to_currency: string; amount: string;}>([
+    {
+        type: 'list',
+        name: 'from_currency',
+        message: 'Select the currency to convert from:',
+        choices: [
+            { name: 'USD', value: 'USD' },
+            { name: 'EUR', value: 'EUR' },
+            { name: 'JPY', value: 'JPY' },
+            { name: 'CAD', value: 'CAD' },
+            { name: 'AUD', value: 'AUD' },
+            { name: 'PAK', value: 'PAK' }
+        ],
+    },
+    {
+        type: 'list',
+        name: 'to_currency',
+        message: 'Select the currency to convert into:',
+        choices: [
+            { name: 'USD', value: 'USD' },
+            { name: 'EUR', value: 'EUR' },
+            { name: 'JPY', value: 'JPY' },
+            { name: 'CAD', value: 'CAD' },
+            { name: 'AUD', value: 'AUD' },
+            { name: 'PAK', value: 'PAK' }
+        ],
+    },
+    {
+        type: 'input',
+        name: 'amount',
+        message: 'Enter the amount to convert:',
+        validate: (value: string) => !isNaN(Number(value)) || 'Please enter a valid number',
+    }
+]);
+//Perform currency convertion by using formula
+
+let fromAmount = exchangeRates[userAnswer.from_currency];
+let to_amount = exchangeRates[userAnswer.to_currency];
+let amount = parseFloat(userAnswer.amount)
+
+//Formula of conversion
+
+let baseAmount = amount/fromAmount
+let convertedAmount = baseAmount * to_amount
+
+//Display the converted amount
+
+console.log(`Converted amount = ${convertedAmount}`);
